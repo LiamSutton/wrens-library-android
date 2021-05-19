a7d19742cfaf530389fa76323bc83dbd7b1b6083
Liam Sutton <liamsutton1997@outlook.com>
2021-05-19 13:27:06 +0100

    Linked fragments in nav graph

--- app/src/main/java/com/ls/wrenslibrary/MainLibraryFragment.java
+++ app/src/main/java/com/ls/wrenslibrary/MainLibraryFragment.java
@@ -2,11 +2,16 @@ package com.ls.wrenslibrary;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +65,22 @@ public class MainLibraryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_library, container, false);
+    }
+
+    private NavController navController;
+    private Button addNewBookButton;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        addNewBookButton = (Button)view.findViewById(R.id.btn_add_new_book);
+        addNewBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_mainLibraryFragment_to_addBookFragment);
+            }
+        });
     }
 }
